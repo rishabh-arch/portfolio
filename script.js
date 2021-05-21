1643f51701da730debc5bb1900082a7ae2d2abc0
Rishabh Garg <rishabhgargts@gmail.com>
2021-05-21 12:21:15 +0530

    change navbar

--- script.js
+++ script.js
@@ -10,5 +10,19 @@
 // let rot = (((radian *(180/Math.PI)*-1)+270));
 // eye.style.transform = `rotate(${rot}deg)`;
 // })
-
 // }
+
+    const buddha_svg = document.getElementById('buddha_svg');
+
+    buddha_svg.classList.replace('buddha_svg_top', 'buddha_svg_top_Active');
+
+    function skill(getid){
+        const unactive = document.querySelectorAll(".page-link");
+        for (var i = 0; i < unactive.length; ++i) {
+            unactive[i].classList.remove('link_bottom_active');
+        }
+        const active = document.getElementById(getid);
+    buddha_svg.classList.replace('buddha_svg_top_Active', 'buddha_svg_top_skill');
+    active.classList.add('link_bottom_active');
+
+    }
