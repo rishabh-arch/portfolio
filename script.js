77eff1241fe3608544ce84156df2a68adefe0d78
Rishabh Garg <rishabhgargts@gmail.com>
2021-05-21 17:59:20 +0530

    change navbar

--- script.js
+++ script.js
@@ -26,3 +26,20 @@
     active.classList.add('link_bottom_active');
 
     }
+
+    // (function(elementSelector) {
+    //     var dragStartX, dragStartY; var objInitLeft, objInitTop;
+    //     var inDrag = false;
+    //     var dragTarget = document.querySelector(elementSelector);
+    //     dragTarget.addEventListener("mousedown", function(e) {
+    //       inDrag = true;
+    //       objInitLeft = dragTarget.offsetLeft; objInitTop = dragTarget.offsetTop;
+    //       dragStartX = e.pageX; dragStartY = e.pageY;
+    //     });
+    //     document.addEventListener("mousemove", function(e) {
+    //       if (!inDrag) {return;}
+    //       dragTarget.style.left = (objInitLeft + e.pageX-dragStartX) + "px";
+    //       dragTarget.style.top = (objInitTop + e.pageY-dragStartY) + "px";
+    //     });
+    //     document.addEventListener("mouseup", function(e) {inDrag = false;});
+    //   }("#bulding"))
